5e42343321488ab825d26a42c3b76cd5bfbbe22d
iliarocks <parun.ilia@gmail.com>
2025-09-11 21:40:55 -0700

    font constants

---
+++ src/constants/font-size.ts
@@ -0,0 +1,7 @@
+const FONT_SIZE = {
+	sm: "14px",
+	md: "16px",
+	lg: "18px",
+};
+
+export default FONT_SIZE;
---
+++ src/constants/line-height.ts
@@ -0,0 +1,7 @@
+const LINE_HEIGHT = {
+	sm: "20px",
+	md: "24px",
+	lg: "24px",
+};
+
+export default LINE_HEIGHT;
