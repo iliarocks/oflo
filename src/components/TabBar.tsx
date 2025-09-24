c17ee262afc40fe9f413d1e4428d4e8777b2970a
iliarocks <parun.ilia@gmail.com>
2025-09-24 10:10:51 -0700

    Working tab bar

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,32 +1,56 @@
-import { View } from "react-native";
-import Button from "@/components/Button";
-import { usePathname, useRouter } from "expo-router";
+import { MenuView, NativeActionEvent } from "@react-native-menu/menu";
+import { Pressable, View } from "react-native";
+import { useRouter } from "expo-router";
+import { SymbolView } from "expo-symbols";
+import COLOR from "@/constants/color";
+import { useContext } from "react";
+import { AuthContext } from "@/contexts/AuthContext";
+
+const navigationMenuItems = [
+	{ id: "/upcoming", title: "Upcoming" },
+	{ id: "/", title: "Today" },
+	{ id: "/inbox", title: "Inbox", imageSystemName: "tray" },
+];
+
+const extraMenuItems = [
+	{ id: "signOut", title: "Sign out", attributes: { destructive: true } },
+];
 
 export default function TabBar() {
-  const router = useRouter();
-  const path = usePathname();
+	const { user, signOut } = useContext(AuthContext);
+	const router = useRouter();
+	const iconProps = { tintColor: COLOR["text-0"], size: 24 };
 
-  const openCreate = () => router.navigate("/create");
+	const handleNavigation = ({ nativeEvent }: NativeActionEvent) => {
+		router.navigate(nativeEvent.event);
+	};
 
-  const goToIndex = () => router.navigate("/");
-  const goToInbox = () => router.navigate("/inbox");
-  const goToUpcoming = () => router.navigate("/upcoming");
-  const goToMore = () => router.navigate("/more");
+	const handleExtra = ({ nativeEvent }: NativeActionEvent) => {
+		if (nativeEvent.event === "signOut") signOut();
+	};
 
-  const isIndex = path === "/";
-  const isInbox = path === "/inbox";
-  const isUpcoming = path === "/upcoming";
-  const isMore = path === "/more";
+	const handleCreate = () => router.navigate("/create");
 
-  return (
-    <View className="flex-row justify-between">
-      <View className="h-3xl flex-row rounded-full bg-neutral-50">
-        <Button icon="tray" onPress={goToInbox} active={isInbox} />
-        <Button icon="sun.max" onPress={goToIndex} active={isIndex} />
-        <Button icon="calendar" onPress={goToUpcoming} active={isUpcoming} />
-        <Button icon="ellipsis" onPress={goToMore} active={isMore} />
-      </View>
-      <Button icon="plus" onPress={openCreate} />
-    </View>
-  );
+	return (
+		<View className="w-full bg-neutral-50 absolute bottom-0 pb-safe">
+			<View className="flex-row justify-between pt-xl pb-md px-4xl">
+				<MenuView
+					onPressAction={handleNavigation}
+					actions={navigationMenuItems}
+				>
+					<SymbolView name="list.bullet" {...iconProps} />
+				</MenuView>
+				<Pressable onPress={handleCreate}>
+					<SymbolView name="square.and.pencil" {...iconProps} />
+				</Pressable>
+				<MenuView
+					onPressAction={handleExtra}
+					title={user?.email}
+					actions={extraMenuItems}
+				>
+					<SymbolView name="ellipsis" {...iconProps} />
+				</MenuView>
+			</View>
+		</View>
+	);
 }
